06b5b6296c91030423da041923e849d4c7dfb51d
Nana Quame <sekyereandy@gmail.com>
2019-12-22 00:57:50 -0500

    Implementing flags and error/exception handling

--- pingv2.py
+++ pingv2.py
@@ -1,64 +1,95 @@
 #!/usr/bin/python
-import sys
-import platform
+# Lint as Python3
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import json
+import logging
+import os
 import subprocess
-import datetime
+import sys
 import time
 
-num = 4
-os = platform.system()
+from absl import app
+from absl import flags
 
-host_list = ['google.com',
-             'facebook.com',
-             'amazon.com',
-             'microsoft.com']
+FLAGS = flags.FLAGS
 
-def timer():
-    for remaining in range(30, 0, -1):
-        sys.stdout.write("\r")
-        sys.stdout.write("{:2d} seconds remaining...".format(remaining))
-        sys.stdout.flush()
-        time.sleep(1)
+flags.DEFINE_string('host', None, 'Website to ping')
+flags.DEFINE_integer('count', 4, 'number of icmp packets to send')
+flags.DEFINE_string('report', None, 'location to store ping result')
 
-while True:
-    for host in host_list:
-        def log_writer(output, start_time):
-            with open("ping_stat.txt", "a") as sFile:
-                sFile.write(str(start_time) + "\n")
-            with open("ping_stat.txt", "ab") as sFile:
-                sFile.write(output)
+flags.mark_flag_as_required('host')
 
-        def ping():
-            start_time = datetime.datetime.now()
-            print(start_time)
-            print("Testing connectivity to " + host)
+class Error(Exception):
+  """Container class for exception in this module."""
 
-            if os == 'Windows' or os =='Win32':
-                pingResults = ['ping', host]
 
-            elif [(os == 'Unix') or (os == 'Darwin') 
-                  or (os == 'Linux') or (os == 'linux2')]:
-                pingResults = ['ping', host, "-c", "{}".format(num)]
+class FileError(Exception):
+  """Error storing output to provided path"""
 
-            else:
-                print("Unknown operatint system")
-                sys.exit()
 
-            ping = subprocess.Popen (pingResults, stdout = subprocess.PIPE,
-                                     stderr = subprocess.PIPE)
+class RpcError(Exception):
+  """Error sending/receiving RPC Stream information"""
 
-            ping.wait()
-            output = ping.stdout.read()
-            error = ping.stderr.read()
+def os_finder():
+  """Check what operating system this script is running on."""
+  try:
+   os_type = sys.platform
+  except ValueError as e:
+      logging.error('Not able to determine OS: %s' %e)
 
-            print(output)
-            log_writer(output, start_time)
+  return os_type
 
-            end_time = str(datetime.datetime.now() - start_time)
-            print("Elapsed time: " + end_time + "\n")
+def countdown_timer():
+  """A sleep timer showing a countdown until next execution"""
+  for remaining in range(30, 0, -1):
+      sys.stdout.write("\r")
+      sys.stdout.write("{:2d} seconds remaining...".format(remaining))
+      sys.stdout.flush()
+      time.sleep(1)
 
-        while True:
-            ping()
-            timer()
-            break
-        continue
+
+class ping_script:
+  """Based on os, host, count and other parameters, execute the ping script"""
+  def __init__(self):
+    self.host = FLAGS.host
+    self.count = FLAGS.count
+
+  def ping_command(self):
+    """Run os_finder function to find out specific os and run command"""
+    os_result = os_finder()
+    if os_result.startswith(('linux2', 'linux', 'Linux', 'Darwin')):
+      logging.info('Executing script on a Linux Workstation')
+      pingResult = ['ping', self.host, "-c", "{}".format(self.count)]
+
+    elif os_result.startswith(('Windows', 'Win32')):
+      logging.info('Executing script on Windows Rig')
+      pingResult = ['ping' + self.host]
+
+    else:
+      logging.error('OS Not Found')
+      sys.exit()
+
+    ping_execute = subprocess.Popen(pingResult, stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
+    
+    output = (ping_execute.stdout.read().decode("utf-8"))
+    error = ping_execute.stderr.read()
+
+    if output:
+      print(output)
+    if error:
+      print(error)
+
+
+def main(argv):
+  if FLAGS.host:
+    logging.info('initiating a ping to %s' %FLAGS.host)
+    core = ping_script()
+    core.ping_command()
+
+if __name__ == "__main__":
+    app.run(main)
